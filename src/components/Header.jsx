5304ba1f383bb64af22694a76c94ec1d137c16a5
Aishi Dutta <aishi.dutta@iemlabs.com>
2024-01-17 17:55:33 +0530

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <div className='bg-yellow-500 text-white font-bold font-mclaren text-2xl p-10 '>Keeps</div>
+            </div>
+        );
+    }
+}
+
+export default Header;
