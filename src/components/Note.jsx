81938d9a10ea5288b4f0b2ec14c7b658d5c678e9
Aishi Dutta <aishi.dutta@iemlabs.com>
2024-01-18 17:51:05 +0530

    second commit

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Note extends Component {
-    render() {
-        return (
-            <div>
-                <div className='bg-white rounded-md shadow-md p-10 w-60 m-4 float-left'><h1>Title</h1>
-                <p>Lorem Ipsum</p></div>
-            </div>
-        );
-    }
+function Note(props) {
+  return (
+    <div>
+      <div className="bg-white rounded-md shadow-md p-5 w-60 m-4 float-left">
+        <h1 className="title text-3xl">{props.title}</h1>
+        <p className="content">{props.content}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Note;
